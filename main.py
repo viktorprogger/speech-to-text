def83becde553ac17e4b6a823993f16acd3a5166
viktorprogger <viktorprogger@gmail.com>
2025-01-31 17:14:51 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,221 @@
+import gi
+from dbus import service
+
+gi.require_version("Gtk", "3.0")
+gi.require_version("Notify", "0.7")
+import os
+import queue
+import subprocess
+import threading
+import time
+
+import dbus
+import numpy as np
+import sounddevice as sd
+import whisper
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import Gtk, Notify
+
+
+class VoiceInputSystem:
+    def __init__(self):
+        # self.setup_tray()
+        os.environ["HSA_OVERRIDE_GFX_VERSION"] = "11.0.0"
+
+        self.model = whisper.load_model(
+            "medium",
+            device="cuda",
+        )
+        # Прогрев модели
+        self.model.transcribe(
+            np.zeros((16000,), dtype=np.float32), language="ru"
+        )  # Пустой аудиосигнал
+        self.recording = False
+        self.audio_queue = queue.Queue()
+        self.text_queue = queue.Queue()
+
+        self.audio_buffer = []
+        self.silence_start = None  # Время начала тишины
+        self.silence_threshold = 0.01  # Порог тишины (можно менять)
+        self.silence_duration = 1.0  # Минимальная длительность паузы (в секундах)
+
+        self.audio_thread = None  # Поток обработки аудио
+        self.stop_event = threading.Event()  # Флаг ожидания сигнала D-Bus
+        self.input_stream = None  # Поток записи микрофона
+
+        # Инициализация уведомлений
+        Notify.init("Voice Input")
+        self.notification = Notify.Notification.new("")
+
+        # Настраиваем D-Bus для приема сигналов от системных хоткеев
+        self.setup_dbus()
+        # self.indicator.set_icon("microphone")
+        # self.indicator.set_title("Voice Input (Inactive)")
+
+    # def setup_tray(self):
+    #     # Создаем индикатор приложения
+    #     self.indicator = AppIndicator3.Indicator.new(
+    #         "viktorprogger-speech-to-text",  # Уникальный идентификатор
+    #         "loading",  # Имя иконки или путь к файлу
+    #         AppIndicator3.IndicatorCategory.APPLICATION_STATUS,
+    #     )
+    #     self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
+
+    #     # Создаем меню для правой кнопки
+    #     self.menu = Gtk.Menu()
+
+    #     # Пункт меню "Quit"
+    #     quit_item = Gtk.MenuItem(label="Quit")
+    #     quit_item.connect("activate", self.on_quit)
+    #     self.menu.append(quit_item)
+
+    #     self.menu.show_all()
+    #     self.indicator.set_menu(self.menu)
+
+    # def on_quit(self):
+    #     Gtk.main_quit()
+
+    def setup_dbus(self):
+        DBusGMainLoop(set_as_default=True)
+        bus = dbus.SessionBus()
+
+        # Регистрируем сервис для приема сигналов
+        bus_name = service.BusName("viktorprogger.home.s2t", bus)
+
+        # Слушаем сигналы от системы
+        bus.add_signal_receiver(
+            self.toggle_recording,
+            dbus_interface="viktorprogger.home.s2t",
+            signal_name="ToggleRecording",
+        )
+
+    def show_notification(self, message):
+        self.notification.update("Voice Input", message, "mic")
+        self.notification.show()
+
+    # def on_right_click(self, icon, button, time):
+    #     menu = Gtk.Menu()
+
+    #     quit_item = Gtk.MenuItem(label="Quit")
+    #     quit_item.connect("activate", self.quit)
+    #     menu.append(quit_item)
+
+    #     menu.show_all()
+    #     menu.popup(None, None, None, None, button, time)
+
+    def quit(self, _):
+        Notify.uninit()
+        Gtk.main_quit()
+        os._exit(0)
+
+    def audio_callback(self, indata, frames, timeinfo, status):
+        if self.recording:
+            volume = np.max(np.abs(indata))  # Определяем громкость
+
+            if volume > self.silence_threshold:
+                self.audio_buffer.append(indata.copy())  # Запоминаем звук
+                self.silence_start = None  # Сбрасываем таймер тишины
+            else:
+                if self.silence_start is None:
+                    self.silence_start = time.time()  # Запоминаем время начала тишины
+
+    def process_audio(self):
+        while self.stop_event.is_set():
+            silence_start = self.silence_start
+            if (
+                silence_start is not None
+                and (time.time() - silence_start) > self.silence_duration
+            ):
+                self.silence_start = None  # Сбрасываем таймер тишины
+                if self.audio_buffer:
+                    # Собираем звук в единый массив
+                    audio = (
+                        np.concatenate(self.audio_buffer).flatten().astype(np.float32)
+                    )
+                    self.audio_buffer = []  # Очищаем буфер после обработки
+
+                    # Отправляем в Whisper
+                    result = self.model.transcribe(audio, language="ru")
+                    text = result["text"]
+
+                    if text.strip() and result["segments"]:
+                        if (
+                            result["segments"][0]["avg_logprob"] > -1.5
+                            and result["segments"][0]["no_speech_prob"] < 0.5
+                        ):
+                            self.text_queue.put(text)  # Отправляем текст в очередь
+                        else:
+                            print(
+                                "Text not sent to queue. logprob: {}, no_speech_prob: {}".format(
+                                    result["segments"][0]["avg_logprob"],
+                                    result["segments"][0]["no_speech_prob"],
+                                )
+                            )
+                            print(text)
+
+                time.sleep(0.5)  # Небольшая пауза в цикле
+
+    def type_text(self):
+        while True:
+            if not self.text_queue.empty():
+                text = self.text_queue.get()
+                try:
+                    # Вводим текст
+                    subprocess.run(
+                        "echo -n '{}' | wl-copy && sleep 0.1 && /home/viktor/.local/bin/ydotool key 29:1 47:1 47:0 29:0".format(
+                            text
+                        ),
+                        shell=True,
+                        check=True,
+                    )
+
+                except subprocess.CalledProcessError as e:
+                    print(f"Error typing text: {e}")
+            time.sleep(0.1)
+
+    def toggle_recording(self):
+        self.recording = not self.recording
+
+        if self.recording:
+            self.show_notification("Voice input activated")
+            self.stop_event.set()  # Разрешаем выполнение потоков
+
+            # Запуск обработки аудио
+            if self.audio_thread is None or not self.audio_thread.is_alive():
+                self.audio_thread = threading.Thread(target=self.process_audio)
+                self.audio_thread.daemon = True
+                self.audio_thread.start()
+
+            # Запуск микрофона
+            if self.input_stream is None:
+                self.input_stream = sd.InputStream(
+                    callback=self.audio_callback, channels=1, samplerate=16000
+                )
+                self.input_stream.start()
+        else:
+            self.show_notification("Voice input deactivated")
+            self.stop_event.clear()  # Останавливаем обработку
+            if self.input_stream:
+                self.input_stream.stop()
+                self.input_stream.close()
+                self.input_stream = None
+
+    def run(self):
+        # Запускаем потоки обработки
+        audio_thread = threading.Thread(target=self.process_audio)
+        audio_thread.daemon = True
+        audio_thread.start()
+
+        text_thread = threading.Thread(target=self.type_text)
+        text_thread.daemon = True
+        text_thread.start()
+
+        # Запускаем запись звука
+        with sd.InputStream(callback=self.audio_callback, channels=1, samplerate=16000):
+            # Запускаем главный цикл GTK
+            Gtk.main()
+
+
+if __name__ == "__main__":
+    vis = VoiceInputSystem()
+    vis.run()
---
+++ readme.md
@@ -0,0 +1,21 @@
+1. `sudo apt install libportaudio2 python3-gi python3-gi-cairo gir1.2-gtk-3.0 libgirepository1.0-dev libcairo2-dev libdbus-1-dev`
+2. Скачать бинари ydotool c https://github.com/ReimuNotMoe/ydotool/releases/latest (в репозитории для Убунту очень старая версия, она не работает)
+3. `python -m venv venv`
+4. `source venv/bin/activate`
+5. `pip install -r requirements.txt`
+6. `pip install torch --index-url https://download.pytorch.org/whl/rocm6.2` - это ставим отдельно, т.к. надо поставить именно ту версию, которая поддерживает мою видюху. В моем случае это прослойка ROCm версии 6.2 для видюх AMD.
+7. Надо создать сервис DBus, чтобы была возможность прослушивать сигнал от хоткея. Создайте файл `/usr/share/dbus-1/services/org.voice.input.service` с таким содержимым:
+  ```
+  [D-BUS Service]
+  Name=viktorprogger.home.s2t
+  Exec=/путь/к/вашему/venv/bin/python3 /путь/к/main.py
+  ```
+7. В настройках Убунту добавить хоткей для команды `dbus-send --session --type=signal /org/voice/input viktorprogger.home.s2t.ToggleRecording`. Я поставил на Ctrl + Shift + X.
+8. Запустить `ydotoold` и `main.py`. Важно делать это от имени своего юзера и в графическом интерфейсе. Все попытки посадить эти команды на автостарт у меня провалились, поэтому я написал себе такую вот функцию для баша:
+  ```bash
+  s2t() {
+    nohup ydotoold &
+    nohup /home/viktor/projects/viktorprogger/speech-to-text/venv/bin/python3 /home/viktor/projects/viktorprogger/speech-to-text/main.py &
+  }
+  ```
+  И теперь пишу в терминале `s2t`, чтобы запустить свой speech to text сервис.
